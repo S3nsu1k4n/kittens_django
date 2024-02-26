9f63c334c92c8b7413b73cd500a06d2664b921cd
S3nsu1k4n <saschakoeppe@gmail.com>
2024-02-26 17:53:58 +0900

    add model description docstring

--- app/models.py
+++ app/models.py
@@ -3,6 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Kitten(models.Model):
+    '''
+    Kitten Model
+
+    Attributes
+    ----------
+    - name : CharField
+    - age : IntegerField
+    - cuteness : IntegerField
+    - softness : IntegerField
+    '''
     name = models.CharField('Name', max_length=32, help_text='Name of the kitten')
     age = models.IntegerField('Age', help_text='Age of the kitten')
     cuteness = models.IntegerField('Cuteness', help_text='Cuteness of the kitten')
