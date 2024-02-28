9faa08b902a4814c21950c7c2e0088985dd06f18
S3nsu1k4n <saschakoeppe@gmail.com>
2024-02-28 14:51:49 +0900

    add create, edit and delete templates

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.http import HttpRequest, HttpResponse
 from django.views import generic
 from . import models
@@ -6,6 +7,7 @@ from . import models
 
 class KittenListView(generic.ListView):
     model = models.Kitten
+    queryset = models.Kitten.objects.all().order_by('pk')
     paginate_by = 20
 
 class KittenDetailView(generic.DetailView):
@@ -13,10 +15,13 @@ class KittenDetailView(generic.DetailView):
 
 class KittenCreate(generic.CreateView):
     model = models.Kitten
+    fields = ['name', 'age', 'cuteness', 'softness']
 
 class KittenUpdate(generic.UpdateView):
     model = models.Kitten
+    fields = ['name', 'age', 'cuteness', 'softness']
 
 class KittenDelete(generic.DeleteView):
     model = models.Kitten
+    success_url = reverse_lazy('index')
     
