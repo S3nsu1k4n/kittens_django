f2789c99276820b55d1c1da502b579d7504a9b57
S3nsu1k4n <saschakoeppe@gmail.com>
2024-02-28 16:49:25 +0900

    add messages to templates for successful/error while create/edit/delete

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.http import HttpRequest, HttpResponse
 from django.views import generic
+from django.contrib import messages
+
 from . import models
 # Create your views here.
 
@@ -17,11 +17,37 @@ class KittenCreate(generic.CreateView):
     model = models.Kitten
     fields = ['name', 'age', 'cuteness', 'softness']
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Kitten created successfully!')
+        return response
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error creating kitten. Please check the form.')
+        return super().form_invalid(form)
+
 class KittenUpdate(generic.UpdateView):
     model = models.Kitten
     fields = ['name', 'age', 'cuteness', 'softness']
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Kitten updated successfully!')
+        return response
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error updating kitten. Please check the form.')
+        return super().form_invalid(form)
 
 class KittenDelete(generic.DeleteView):
     model = models.Kitten
     success_url = reverse_lazy('index')
     
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Kitten deleted successfully!')
+        return response
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error deleting kitten.')
+        return super().form_invalid(form)
